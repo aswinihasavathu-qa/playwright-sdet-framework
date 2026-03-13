accacb3003558834ca4093077f237749503ab6f2
aswinihasavathu <aswinihasavathu97@gmail.com>
2026-03-13 21:16:14 +0530

    Fix loginPage casing issue

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import { LoginPage } from '../pages/loginPage'
+import { LoginPage } from '../pages/temp'
 import users from '../test-data/users.json'
 
 users.users.forEach((user, index) => {
