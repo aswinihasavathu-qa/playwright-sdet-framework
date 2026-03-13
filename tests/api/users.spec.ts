0eb2e7957dcd85d98deafec36f0244ff7c60f507
aswinihasavathu <aswinihasavathu97@gmail.com>
2026-03-13 19:54:59 +0530

    Fixed API response validation

---
+++ tests/api/users.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test('Get Users API Test', async ({ request }) => {
+
+  const response = await request.get('https://jsonplaceholder.typicode.com/users')
+
+  expect(response.status()).toBe(200)
+
+  const body = await response.json()
+
+  console.log(body)
+
+  expect(body.length).toBeGreaterThan(0)
+
+})
