34d6aec03b7298d980190887e2b7075980b61843
aswinihasavathu <aswinihasavathu97@gmail.com>
2026-03-13 15:11:35 +0530

    Added environment config and base fixture

--- tests/login-pom.spec.ts
+++ tests/login-pom.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/loginPage'
+import { ENV } from '../config/env'
 
 test('Login Test Using POM', async ({ page }) => {
 
-const login = new LoginPage(page)
+    const login = new LoginPage(page)
 
-await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.goto(ENV.baseURL)
 
-await login.login('Admin','admin123')
+    await login.login(ENV.username, ENV.password)
 
-// wait for dashboard element
-await expect(page.locator('h6:has-text("Dashboard")')).toBeVisible()
-await expect(page).toHaveURL(/dashboard/)
-
+    await expect(page).toHaveURL(/dashboard/)
 })
